c12c626f20aacb81aeadf84495c2b6e6ff690f01
rushi <rushikeshgovekar3@gmail.com>
2024-10-21 14:37:09 +0530

    commit

---
+++ src/app/appointment/appointment.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-appointment',
+  standalone: true, // This should be true for standalone components
+  templateUrl: './appointment.component.html', // Adjust the path if needed
+  styleUrls: ['./appointment.component.css']
+})
+export class AppointmentComponent {}
